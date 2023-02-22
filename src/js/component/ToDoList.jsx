09f57b4c89a95fc159e4486445cd6fe08539ce98
Georgi254 <georgi.gyovchev254@gmail.com>
2023-02-22 20:41:20 +0000

    Tarea terminada

--- src/js/component/ToDoList.jsx
+++ src/js/component/ToDoList.jsx
@@ -1,49 +1,93 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 //create your first component
 const ToDoList = () => {
   const [inputValue, setInputValue] = useState("");
   const [todos, setTodos] = useState([]);
 
+  let host = "https://assets.breatheco.de/apis/fake/todos/user/";
+  let user = "georgi";
+
+  const agregarTarea = async (e) => {
+    let url = host + user
+    //Aquí vamos a agregar todo lo del input
+    if (inputValue === "") {
+      return;
+    }
+    if (e.key === "Enter") {
+      setTodos([...todos, {
+        "label": inputValue,
+        "done": false
+      }]);
+      setInputValue("");
+      const request = {
+        method: "PUT",
+        redirect: "follow",
+        body: JSON.stringify([...todos, {
+          "label": inputValue,
+          "done": false
+        }]),
+        headers: { "Content-Type": "application/json" },
+      };
+      const response = await fetch(url, request);
+    }
+    //Hacer el PUT. (Generar de vuelta un nuevo array y agregarle de vuelta la nueva)
+  
+  };
+
+  async function devolverTareas()  {
+    const url = host + user;
+    const request = {
+      method: "GET",
+      redirect: "follow",
+    };
+    const response = await fetch(url, request);
+    if (response.ok) {
+      const responseJSON = await response.json();
+      console.log(responseJSON)
+      setTodos(responseJSON)
+      
+      
+    } else {
+      console.log("error", error);
+    }
+  }
+
+  //Aquí voy a generar el useEffect() para que se renderice la lista
+
+  useEffect(() => {
+    devolverTareas();
+  }, []);
+
   return (
     <div className="container">
       <h1>My To-Do List</h1>
+      <input
+        type="text"
+        onChange={(e) => setInputValue(e.target.value)}
+        value={inputValue}
+        onKeyPress={agregarTarea}
+        placeholder="What do you need to do?"
+      ></input>
+
       <ul>
-        <li>
-          <input
-            type="text"
-            onChange={(e) => setInputValue(e.target.value)}
-            value={inputValue}
-            onKeyPress={(e) => {
-              if (e.key === "Enter") {
-                setTodos(todos.concat(inputValue));
-                setInputValue("");
-              }
-            }}
-            placeholder="What do you need to do?"
-          ></input>
-        </li>
         {todos.map((item, index) => (
-          <li className="d-flex justify-content-between hidden-icon">
-            {item}{" "}
-            <span>
-              <i
-                class="far fa-trash-alt"
-                onClick={() =>
-                  setTodos(
-                    todos.filter((t, currentIndex) => index != currentIndex)
-                  )
-                }
-              ></i>
-            </span>
+          <li
+            key={index}
+            className="d-flex justify-content-between hidden-icon"
+          >
+            {item.label}
           </li>
         ))}
-        <div className="task-left">
-        <span>
-              {todos.length === 0 ? "No tasks, add a task" : todos.length + " Item Left"}
-          </span>
-        </div>
       </ul>
+
+      <div className="task-left">
+        <span>
+          {todos.length === 0
+            ? "No tasks, add a task"
+            : todos.length + " Item Left"}
+        </span>
+      </div>
     </div>
   );
 };
